62b8041470cdac1eb45f15808f7360ead9232a4d
comacrae <comacrae1995@gmail.com>
2025-04-16 16:44:49 -0500

    going to try to update tsconfig

---
+++ server/src/services/userService.ts
@@ -0,0 +1,4 @@
+import mongoose from "mongoose";
+import User from "../models/User";
+
+const getUsers: 
---
+++ server/tests/utils.ts
@@ -0,0 +1,20 @@
+import { getMongoClient } from "../src/db/mongoConnector";
+import fs from "fs";
+import mongoose from "mongoose";
+
+const getTestDeps = (path: string = `tests/tests_deps.json`) => {
+  const json = fs.readFileSync(path, "utf-8");
+  const parsed = JSON.parse(json);
+  return parsed;
+};
+
+const teardownMongoClient = () => {
+  mongoose.connection.close();
+};
+
+const connectMongoose = () => {
+  const uri: string = getTestDeps(`tests/tests_deps.json`).MONGO_URI;
+  getMongoClient(uri);
+};
+
+export { getTestDeps, teardownMongoClient, connectMongoose };
