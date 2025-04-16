a809180d55397a6c88cd306e71d7eeeee8e6ed52
comacrae <comacrae1995@gmail.com>
2025-04-16 13:33:42 -0500

    trying to get mongoose set up

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,4 +1,7 @@
-export interface User {
-  id: number;
-  name: string;
-}
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema({
+  username: String,
+  email: String,
+});
