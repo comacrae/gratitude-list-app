b351b7ddf45528da002e89ce6cca02e5b788e893
comacrae <comacrae1995@gmail.com>
2025-04-17 09:40:15 -0500

    updated user schema

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -4,11 +4,17 @@ const { Schema, model } = mongoose;
 interface IUser {
   username: string;
   email: string;
+  following: string[];
+  followers: string[];
+  joined: Date;
 }
 
 const userSchema = new Schema<IUser>({
   username: { type: String, required: true },
   email: { type: String, required: true },
+  following: { type: [String], required: false, default: [] },
+  followers: { type: [String], required: false, default: [] },
+  joined: { type: Date, required: true, default: Date.now },
 });
 
 const User = model<IUser>("User", userSchema);
--- server/tests/userService.test.ts
+++ server/tests/userService.test.ts
@@ -19,3 +19,35 @@ test("User Model invalid init", () => {
   const user = new User({ username: null, email: 123 });
   expect(user.validate()).rejects.toThrow();
 });
+
+test("User Model CRUD success", async () => {
+  const mockUser = new User({
+    username: "mockusername",
+    email: "mockemail",
+  });
+
+  //create
+  await mockUser.save();
+
+  //read
+  const queriedUser = await User.findOne({ username: "mockusername" }).exec();
+  expect(mockUser.username).toBe(queriedUser?.username);
+
+  //update
+  if (queriedUser !== null) {
+    queriedUser.email = "newmockemail";
+    await queriedUser.save();
+  }
+
+  const updatedUser = await User.findOne({ username: "mockusername" }).exec();
+
+  expect(updatedUser?.email).toBe("newmockemail");
+
+  //delete using deleteMany to be safe
+  await User.deleteMany({
+    username: "mockusername",
+  }).exec();
+
+  const shouldBeNull = await User.findOne({ username: "mockusername" }).exec();
+  expect(shouldBeNull).toBeNull();
+});
